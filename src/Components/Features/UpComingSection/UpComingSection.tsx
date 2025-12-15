197ad61957e18cecf3d344891a2a6fa95996b1e3
Hovik Poghosyan <hovik_poghosyan@epam.com>
2025-12-15 16:48:26 +0400

    realise

--- src/Components/Features/UpComingSection/UpComingSection.tsx
+++ src/Components/Features/UpComingSection/UpComingSection.tsx
@@ -17,10 +17,10 @@ import 'react-alice-carousel/lib/scss/alice-carousel.scss';
 
 function UpComingSection() {
    const itemsList = [ 
-      <ConcertCard title = "Violin Concerts" time='18 : 00' location='Gyumri, Ozanyan 1 (Hayordyats tun)' imageUrl='\Images\image2.jpg' data='December 16' members={[{ position: 'Conductor', name: 'Armen Kartchyan'}, { position: 'Soloist', name: 'Artyom Richagov (violin)'}, { position: 'Soloist', name: 'Armen Badalyan (violin)'}]} />, 
-      <ConcertCard title = "Violin Concerts" time='18 : 00' location='Vanadzor, Tigran Metsi Ave. 2' imageUrl='\Images\image2.jpg' data='December 17' members={[{ position: 'Conductor', name: 'Armen Kartchyan'}, { position: 'Soloist', name: 'Artyom Richagov (violin)'}, { position: 'Soloist', name: 'Armen Badalyan (violin)'}]} />, 
-      <ConcertCard title = "Cello Evening" time='18 : 00' location='Gyumri, Ozanyan 1(Hayordyats tun)' imageUrl='\Images\image3.jpg' data='December 21' members={[{ position: 'Conductor', name: 'Armen Kartchyan'}, { position: 'Soloist', name: 'Lyana Ulikhanyan (cello)'}, { position: 'Soloist', name: 'Levon Arakelyan (cello)'}]} />, 
-      <ConcertCard title = "New Years Miracle" time='14 : 00' location='Gyumri, Ozanyan 1(Hayordyats tun)' imageUrl='\Images\image4.jpg' data='December 25' members={[{ position: 'Conductor', name: 'Ruben Asatryan'}, ]} />, 
+      <ConcertCard title = "Violin Concerts" time='18 : 00' location='Gyumri, Ozanyan 1 (Hayordyats tun)' imageUrl='\Images\image2.jpg' data='December 16' members={[{ position: 'conductor', name: 'Armen Kartchyan'}, { position: 'soloist', name: 'Artyom Richagov (violin)'}, { position: 'soloist', name: 'Armen Badalyan (violin)'}]} />, 
+      <ConcertCard title = "Violin Concerts" time='18 : 00' location='Vanadzor, Tigran Metsi Ave. 2' imageUrl='\Images\image2.jpg' data='December 17' members={[{ position: 'conductor', name: 'Armen Kartchyan'}, { position: 'soloist', name: 'Artyom Richagov (violin)'}, { position: 'soloist', name: 'Armen Badalyan (violin)'}]} />, 
+      <ConcertCard title = "Cello Evening" time='18 : 00' location='Gyumri, Ozanyan 1(Hayordyats tun)' imageUrl='\Images\image3.jpg' data='December 21' members={[{ position: 'conductor', name: 'Armen Kartchyan'}, { position: 'soloist', name: 'Lyana Ulikhanyan (cello)'}, { position: 'soloist', name: 'Levon Arakelyan (cello)'}]} />, 
+      <ConcertCard title = "New Years Miracle" time='14 : 00' location='Gyumri, Ozanyan 1(Hayordyats tun)' imageUrl='\Images\image4.jpg' data='December 25' members={[{ position: 'conductor', name: 'Ruben Asatryan'}, ]} />, 
    ]
    return(
       <section
